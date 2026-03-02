63136e8de78c91f18c7cdf1f051819ba41c4bf53
rexroxen <amit@ebizondigital.com>
2026-03-02 10:11:33 +0530

    Fix index redirect loop 9v

--- app/routes/auth.login/route.jsx
+++ app/routes/auth.login/route.jsx
@@ -1,11 +1,19 @@
 import { redirect } from "react-router";
+import { login } from "../../shopify.server";
+import { loginErrorMessage } from "./error.server";
 
-export const loader = async () => {
-  throw redirect("/app/reviews");
+export const loader = async ({ request }) => {
+  const errors = loginErrorMessage(await login(request));
+
+  if (Object.keys(errors).length === 0) {
+    throw redirect("/app/reviews");
+  }
+
+  return { errors };
 };
 
-export const action = async () => {
-  throw redirect("/app/reviews");
+export const action = async ({ request }) => {
+  return login(request);
 };
 
 export default function Auth() {
